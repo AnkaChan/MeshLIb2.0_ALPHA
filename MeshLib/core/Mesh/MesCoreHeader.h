2ba5131c4a74d4ebe625f2f1eb49ff5052d6c1d0
ssk <sskqgfnnh@gmail.com>
2017-07-29 22:51:04 +0800

    modify

---
+++ MeshLib/core/Mesh/MesCoreHeader.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <MeshLib/core/Mesh/BaseMesh.h>
+#include <MeshLib/core/Mesh/Interface.h>
+#include <MeshLib/core/Mesh/Vertex.h>
+#include <MeshLib/core/Mesh/HalfEdge.h>
+#include <MeshLib/core/Mesh/Edge.h>
+#include <MeshLib/core/Mesh/Face.h>
+#include <MeshLib/core/Mesh/iterators.h>
+#include <MeshLib/core/Mesh/Property.h>
