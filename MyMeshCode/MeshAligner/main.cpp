66692bb4462d4f29e8325d0dcdec159b1e4ecb19
AnkaChan <ankachan@126.com>
2017-10-11 16:00:30 +0800

    Signed-off-by: AnkaChan <ankachan@126.com>

---
+++ MyMeshCode/MeshAligner/main.cpp
@@ -0,0 +1,16 @@
+#include <MeshLib/core/Mesh/MeshCoreHeader.h>
+#include <MeshLib/core/Mesh/boundary.h>
+//#include <MeshLib/core/viewer/SimpleMeshViewer.h>
+#include <MeshLib/toolbox/FileIO.h>
+#include <iostream>
+#include <set>
+using std::cout; 
+using std::endl;
+using namespace MeshLib;
+
+typedef CInterface<CVertex, CEdge, CFace, CHalfEdge> If;
+typedef CIterators<If> It;
+
+int main(int argc, char** argv) {
+
+}
